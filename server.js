1c8ce0ba526881cb7458a5e89fca7bbae49ac8f3
marcipanis <yuliya-mazko@yandex.ru>
2022-10-14 16:50:53 +0300

    X

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-import path from 'path';
-
-import server from 'express';
-
+const path = require("path");
+const server = require("express");
 const app = server();
 const port = process.env.PORT || 3000;
 
@@ -10,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(server.static(path.join(__dirname, '/dist')));
 
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/dist/index.html`);
+  res.status(200).sendFile(`${__dirname}/dist/index.html`);
 });
 
 app.get('/login.html', (req, res) => {
@@ -18,31 +16,31 @@ app.get('/login.html', (req, res) => {
 });
 
 app.get('/registration.html', (req, res) => {
-  res.sendFile(`${__dirname}/dist/index.html`);
+  res.status(200).sendFile(`${__dirname}/dist/index.html`);
 });
 
 app.get('/chat.html', (req, res) => {
-  res.sendFile(`${__dirname}/dist/index.html`);
+  res.status(200).sendFile(`${__dirname}/dist/index.html`);
 });
 
 app.get('/account.html', (req, res) => {
-  res.sendFile(`${__dirname}/dist/index.html`);
+  res.status(200).sendFile(`${__dirname}/dist/index.html`);
 });
 
 app.get('/account_change_password.html', (req, res) => {
-  res.sendFile(`${__dirname}/dist/index.html`);
+  res.status(200).sendFile(`${__dirname}/dist/index.html`);
 });
 
 app.get('/account_edit.html', (req, res) => {
-  res.sendFile(`${__dirname}/dist/index.html`);
+  res.status(200).sendFile(`${__dirname}/dist/index.html`);
 });
 
 app.get('/error500.html', (req, res) => {
-  res.sendFile(`${__dirname}/dist/index.html`);
+  res.status(500).sendFile(`${__dirname}/dist/index.html`);
 });
 
 app.get('/error.html', (req, res) => {
-  res.sendFile(`${__dirname}/dist/index.html`);
+  res.status(404).sendFile(`${__dirname}/dist/index.html`);
 });
 
 app.listen(port, () => {
